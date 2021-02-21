04070d13eb64958e6038f0fdb5b167bbf16a18c8
Mateus Alencar <mtauws@gmail.com>
2021-02-21 11:50:33 -0300

    feat: exe01.js

---
+++ exercises/bloco_4/dia_01/exe01.js
@@ -0,0 +1,10 @@
+const name = 'Mateus';
+const birthCity = 'Juazeiro do Norte';
+let birthYear = 2000;
+
+birthYear = 2020;
+birthCity = 'Vancouver';
+
+console.log(name);
+console.log(birthCity);
+console.log(birthYear);
