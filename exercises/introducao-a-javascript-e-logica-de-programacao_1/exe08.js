0ae0c24c0b815a003eac477744315596ab4b0803
Mateus Alencar <mtauws@gmail.com>
2021-02-21 12:57:14 -0300

    feat: exercise 8

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exe08.js
@@ -0,0 +1,9 @@
+let valueOne = 2;
+let valueTwo = 3;
+let valueThree = 17;
+
+if (valueOne % 2 == 0 || valueTwo % 2 == 0 || valueThree % 2 == 0) {
+  console.log(true)
+} else {
+  console.log(false)
+}
