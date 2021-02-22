ef9a2c0e5e23abd8d2e44cfe34fbf0f9c1c703e4
Mateus Alencar <mtauws@gmail.com>
2021-02-22 11:56:20 -0300

    feat: exercise 5

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/exe05.js
@@ -0,0 +1,10 @@
+let angleOne = 140;
+let angleTwo = 20;
+let angleThree = 10;
+
+const sumOfAngles = angleOne + angleTwo + angleThree;
+if (sumOfAngles == 180) {
+  console.log(true);
+} else {
+  console.log(false);
+}
