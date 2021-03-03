1cb421fa8c41d13ec6026e7873e586c84769d1e2
Mateus Alencar <mtauws@gmail.com>
2021-03-03 17:30:35 -0300

    refactor: block 02 and 05

--- fixation-day/module_01/bloco_4/dia_01/exe03.js
+++
@@ -1,9 +0,0 @@
-let nota = 96;
-
-if (nota >= 80) {
-  console.log('Parabéns, você foi aprovado!');
-} else if (nota < 80 && nota >= 60) {
-  console.log('Você está na nossa lista de espera');
-} else {
-  console.log('Você foi reprovado');
-}
