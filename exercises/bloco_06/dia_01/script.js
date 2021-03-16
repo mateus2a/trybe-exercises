4bed749b1a565e640a4ece2e00478f3b673c58d2
Mateus Alencar <mtauws@gmail.com>
2021-03-16 20:18:46 -0300

    data's validation

--- exercises/bloco_06/dia_01/script.js
+++ exercises/bloco_06/dia_01/script.js
@@ -1,3 +1,25 @@
+// Validando Data
+var picker = new Pikaday({
+  field: document.getElementById('datepicker'),
+  format: 'D/M/YYYY',
+  toString(date, format) {
+      // you should do formatting based on the passed format,
+      // but we will just return 'D/M/YYYY' for simplicity
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+      return `${day}/${month}/${year}`;
+  },
+  parse(dateString, format) {
+      // dateString is the result of `toString` method
+      const parts = dateString.split('/');
+      const day = parseInt(parts[0], 10);
+      const month = parseInt(parts[1], 10) - 1;
+      const year = parseInt(parts[2], 10);
+      return new Date(year, month, day);
+  }
+});
+
 $(document).ready(function() {
   $('#save').click(function() {
     let inputName = $('#name').val();
@@ -9,3 +31,4 @@ $(document).ready(function() {
     $('#paragraph-three').append(inputDate);
   });
 });
+
