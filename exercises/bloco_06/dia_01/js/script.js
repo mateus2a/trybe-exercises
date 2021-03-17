744cff9b2e019bb44626773f425d7476b72a0070
Mateus Alencar <mtauws@gmail.com>
2021-03-16 23:37:06 -0300

    save data in browser

--- exercises/bloco_06/dia_01/js/script.js
+++ exercises/bloco_06/dia_01/js/script.js
@@ -20,15 +20,15 @@ var picker = new Pikaday({
   }
 });
 
-$(document).ready(function() {
-  $('#save').click(function() {
-    let inputName = $('#name').val();
-    let inputEmail = $('#email').val();
-    let inputDate = $('#datepicker').val();
 
-    $('#paragraph-one').append(inputName);
-    $('#paragraph-two').append(inputEmail);
-    $('#paragraph-three').append(inputDate);
-  });
+$('#save').click(function() {
+  let inputName = $('#name').val();
+  let inputEmail = $('#email').val();
+  let inputDate = $('#datepicker').val();
+
+  $('#paragraph-one').append(inputName);
+  $('#paragraph-two').append(inputEmail);
+  $('#paragraph-three').append(inputDate);
 });
+
 
