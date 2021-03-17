5da71c589540c1ada6d79a93e138bde7786fac0e
Mateus Alencar <mtauws@gmail.com>
2021-03-17 00:56:56 -0300

    chore: validations on form

--- exercises/bloco_06/dia_01/js/script.js
+++ exercises/bloco_06/dia_01/js/script.js
@@ -20,15 +20,20 @@ var picker = new Pikaday({
   }
 });
 
+$('#save').click(function(event) {
+  event.preventDefault();
 
-$('#save').click(function() {
   let inputName = $('#name').val();
   let inputEmail = $('#email').val();
   let inputDate = $('#datepicker').val();
 
-  $('#paragraph-one').append(inputName);
-  $('#paragraph-two').append(inputEmail);
-  $('#paragraph-three').append(inputDate);
+    if (inputName == '' || inputEmail == '' || inputDate == '') {
+      alert('Campos vazios');
+    } else {
+      $('#paragraph-one').append(inputName);
+      $('#paragraph-two').append(inputEmail);
+      $('#paragraph-three').append(inputDate);
+    }
 });
 
 
