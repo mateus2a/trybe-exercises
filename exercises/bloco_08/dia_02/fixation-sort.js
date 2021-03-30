b132566090eb6a2edd069e064e0cd852b9589790
Mateus Alencar <mtauws@gmail.com>
2021-03-30 15:34:21 -0300

    some, every and sort

---
+++ exercises/bloco_08/dia_02/fixation-sort.js
@@ -0,0 +1,8 @@
+const food = ['arroz', 'feijão', 'farofa', 'chocolate', 'doce de leite'];
+food.sort();
+console.log(food);
+// [ 'arroz', 'chocolate', 'doce de leite', 'farofa', 'feijão' ]
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort((a, b) => a - b);
+console.log(points); // [1, 5, 10, 25, 40, 100]
