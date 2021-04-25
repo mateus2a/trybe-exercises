7ec4e9ce3389eb799a4cbb4bc0778eafeca64627
Mateus Alencar <mtauws@gmail.com>
2021-04-25 10:47:31 -0300

    album component

--- exercises/modulo_02/bloco_11/dia_02/fixation-exercises-11-2/src/App.js
+++ exercises/modulo_02/bloco_11/dia_02/fixation-exercises-11-2/src/App.js
@@ -1,12 +1,42 @@
 import React from 'react';
 import Image from './Image';
+import Album from './Album';
 import './App.css'
 
 class App extends React.Component {
   render() {
+    const album01 = {
+      image: 'https://upload.wikimedia.org/wikipedia/en/thumb/b/b0/Coldplay_-_Mylo_Xyloto.JPG/220px-Coldplay_-_Mylo_Xyloto.JPG',
+      title: 'Mylo Xyloto',
+      releaseDate: {
+        year: '2011',
+        month: '10',
+        day: '24',
+      },
+      others: {
+        recordCompany: 'Capitol, Parlophone',
+        formats: 'CD, digital'
+      }
+    };
+
+    // Declaração do objeto contendo informações do album02
+    const album02 = {
+      image: 'https://upload.wikimedia.org/wikipedia/pt/thumb/5/5d/Coldplay_-_Ghost_Stories.jpg/220px-Coldplay_-_Ghost_Stories.jpg',
+      title: 'Ghost Stories',
+      releaseDate: {
+        year: '2014',
+        month: '05',
+        day: '16',
+      },
+      others: {
+        recordCompany: 'Parlophone',
+        formats: 'CD, digital'
+      }
+    };
     return (
-      <div className="App">
-        <Image id="img" source={'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'} alternativeText={'Cute Cat'}/>
+      <div>
+        <Album album={ album01 }/>
+        <Album album={ album02 }/>
       </div>
     );
   }
