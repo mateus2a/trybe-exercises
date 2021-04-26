c5125278f3739ca573ce553682761aa29aac85c0
Mateus Alencar <mtauws@gmail.com>
2021-04-26 09:54:12 -0300

    map

--- exercises/modulo_02/bloco_11/dia_02/fixation-exercises-11-2/src/App.js
+++ exercises/modulo_02/bloco_11/dia_02/fixation-exercises-11-2/src/App.js
@@ -1,35 +1,16 @@
-// arquivo App.js, criado pelo create-react-app, modificado
 import React from 'react';
-import './App.css';
-import Order from './Order';
 
 class App extends React.Component {
   render() {
-    const headphone = {
-      id: 102,
-      user: "cena@gmail.com",
-      product: "Razer Headphone",
-      price: {
-        value: 99.99,
-        currency: "dollars"
-      }
-    };
-
-    const energyDrink = {
-      id: 77,
-      user: "cena@gmail.com",
-      product: "Monster 500mL",
-      price: {
-        value: 9.99,
-        currency: "dollars"
-      }
-    };
+    const shoppingList = ['leite', 'arroz', 'feijão', 'banana', 'carne'];
+    const items = shoppingList.map((item, index) => (<li key={ index }>{ item }</li>));
 
     return (
-      <div className="App">
-        <h1> Orders recently created </h1>
-         <Order order={headphone}/>
-         <Order order={energyDrink}/>
+      <div>
+        <h2>Lista de compras</h2>
+        <ul>
+          { items }
+        </ul>
       </div>
     );
   }
