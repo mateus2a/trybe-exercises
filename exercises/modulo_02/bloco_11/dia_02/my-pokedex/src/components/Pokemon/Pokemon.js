40123ef30bba010d3601146a33185ad10aae81d1
Mateus Alencar <mtauws@gmail.com>
2021-04-27 12:52:42 -0300

    PropTypes

--- exercises/modulo_02/bloco_11/dia_02/my-pokedex/src/components/Pokemon/Pokemon.js
+++ exercises/modulo_02/bloco_11/dia_02/my-pokedex/src/components/Pokemon/Pokemon.js
@@ -1,12 +1,11 @@
 import React from 'react';
-
-import pokemons from '../../database/data';
+import PropTypes from 'prop-types';
 
 import './pokemon.css';
 
 class Pokemon extends React.Component {
     render() {
-        const { name, type, averageWeight, image } = this.props.pokemon;
+        const { name, type, averageWeight, image } = this.props.pokemons;
 
         return (
             <ul className="container">
@@ -20,5 +19,17 @@ class Pokemon extends React.Component {
         );
     }
 }
+
+Pokemon.propTypes = {
+    pokemon: PropTypes.shape({
+        name: PropTypes.string,
+        type: PropTypes.string,
+        averageWeight: PropTypes.shape({
+            value: PropTypes.number,
+            measurementUnit: PropTypes.string,
+        }),
+        image: PropTypes.string,
+    }).isRequired,
+};
 
 export default Pokemon;
