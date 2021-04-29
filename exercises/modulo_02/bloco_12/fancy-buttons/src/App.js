ee2f4886c794fe421cce6e2325f0474a21c96f95
Mateus Alencar <mtauws@gmail.com>
2021-04-29 16:01:11 -0300

    chore: fixation

--- exercises/modulo_02/bloco_12/fancy-buttons/src/App.js
+++ exercises/modulo_02/bloco_12/fancy-buttons/src/App.js
@@ -4,6 +4,7 @@ class App extends React.Component {
   constructor() {
     super()
 
+    this.isPair = this.isPair.bind(this);
     this.handleButtonOne = this.handleButtonOne.bind(this);
     this.handleButtonTwo = this.handleButtonTwo.bind(this);
     this.handleButtonThree = this.handleButtonThree.bind(this);
@@ -15,28 +16,43 @@ class App extends React.Component {
     }
   }
 
+  isPair(number) {
+    if (number % 2 !== 0) {
+      document.querySelector('.btnOne').style.background = 'green';
+      console.log('green');
+    } else {
+      document.querySelector('.btnOne').style.background = '#efefee';
+    }
+  }
+
   handleButtonOne() {
     this.setState((beforeState, _props) => ({
       numberOne: beforeState.numberOne + 1,
     }));
+
+    this.isPair(this.state.numberOne);
   }
 
   handleButtonTwo() {
     this.setState((beforeState, _props) => ({
       numberTwo: beforeState.numberTwo + 2,
     }));
+    
+    this.isPair(this.state.numberTwo);
   }
 
   handleButtonThree() {
     this.setState((beforeState, _props) => ({
       numberThree: beforeState.numberThree + 3,
     }));
+
+    this.isPair(this.state.numberThree);
   }
 
   render() {
     return (
       <div>
-        <button onClick={this.handleButtonOne}>{this.state.numberOne}</button>
+        <button className="btnOne" onClick={this.handleButtonOne}>{this.state.numberOne}</button>
         <button onClick={this.handleButtonTwo}>{this.state.numberTwo}</button>
         <button onClick={this.handleButtonThree}>{this.state.numberThree}</button>
       </div>
