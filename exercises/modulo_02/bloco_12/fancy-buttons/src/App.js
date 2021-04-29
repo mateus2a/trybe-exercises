22bebd4dc25555c2e46183d669373d1eecf90ece
Mateus Alencar <mtauws@gmail.com>
2021-04-29 15:13:58 -0300

    chore: understanding this

--- exercises/modulo_02/bloco_12/fancy-buttons/src/App.js
+++ exercises/modulo_02/bloco_12/fancy-buttons/src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 
-function bomDia() {
-  return console.log('Bom dia');
-}
-
-function boaTarde() {
-  return console.log('Boa Tarde');
-}
-
-function boaNoite() {
-  return console.log('Boa Noite');
-}
-
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.bomDia = this.bomDia.bind(this);
+    this.boaTarde = this.boaTarde.bind(this);
+    this.boaNoite = this.boaNoite.bind(this);
+    console.log('Constructor');
+  }
+
+  bomDia() {
+    return console.log('Bom dia');
+  }
+  
+  boaTarde() {
+    return console.log('Boa Tarde');
+  }
+  
+  boaNoite() {
+    return console.log('Boa Noite');
+  }
+
   render() {
     return (
       <div className="App">
-        <button onClick={bomDia}>Bom Dia</button>
-        <button onClick={boaTarde}>Boa Tarde</button>
-        <button onClick={boaNoite}>Boa Noite</button>
+        <button onClick={this.bomDia}>Bom Dia</button>
+        <button onClick={this.boaTarde}>Boa Tarde</button>
+        <button onClick={this.boaNoite}>Boa Noite</button>
       </div>
     );
   }
