58dcdc15532fdad4b6ffc48fc834db0239af30e1
Mateus Alencar <mtauws@gmail.com>
2021-04-30 15:40:36 -0300

    event handle generico

--- exercises/modulo_02/bloco_12/fixation-12-2/src/components/Form.js
+++ exercises/modulo_02/bloco_12/fixation-12-2/src/components/Form.js
@@ -1,73 +1,63 @@
 import React, { Component } from 'react'
 
 class Form extends Component {
-  constructor() {
-    super();
-
-    this.handleName = this.handleName.bind(this);
-    this.handleSong = this.handleSong.bind(this);
-    this.handleNumber = this.handleNumber.bind(this);
-    this.handleHistory = this.handleHistory.bind(this);
+  constructor(props) {
+    super(props);
     this.state = {
-      song: '',
-      name: '',
-      number: 0,
-      history: '',
-    }
+      isGoing: true,
+      numberOfGuests: 2,
+      file: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleName(event) { 
-    this.setState({
-      name: event.target.value,
-    })
-  }
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
 
-  handleSong(event) { 
     this.setState({
-      song: event.target.value,
-    })
-  }
-
-  handleHistory(event) { 
-    this.setState({
-      history: event.target.value,
-    })
-  }
-
-  handleNumber(event) { 
-    this.setState({
-      number: event.target.value,
-    })
+      [name]: value
+    });
   }
 
   render() {
     return (
-      <div className="form-container">
-        <select name="select" id="select" value={this.state.song} onChange={this.handleSong}>
-          <option value="k-pop">K-Pop</option>
-          <option value="rock">Rock</option>
-          <option value="blues">Blues</option>
-        </select>
-        <label htmlFor="name">
-          Nome:
-          <input type="text" id="name" value={this.state.name} onChange={this.handleName}/>
+      <form>
+        <fieldset>
+          <legend>Form</legend>
+        <label>
+          Estão indo:
+          <input
+            name="isGoing"
+            type="checkbox"
+            checked={this.state.isGoing}
+            onChange={this.handleInputChange} />
         </label>
-        <label htmlFor="number">
-          Numero:
-          <input type="number" id="number" value={this.state.number} onChange={this.handleNumber}/>
+        <br />
+        <label>
+          Número de convidados:
+          <input
+            name="numberOfGuests"
+            type="number"
+            value={this.state.numberOfGuests}
+            onChange={this.handleInputChange} />
         </label>
-        <label htmlFor="your-history">
-          <textarea 
-            name="your-history" 
-            id="your-history" 
-            cols="30" rows="10" 
-            value={this.state.history} 
-            onChange={this.handleHistory}>
-          </textarea>
+
+        <label>
+          Arquivo:
+          <input
+            name="updateAFile"
+            type="file"
+            value={this.state.file}
+            onChange={this.handleInputChange} />
         </label>
-      </div>
+        </fieldset>
+      </form>
     );
   }
 }
+
 
 export default Form;
