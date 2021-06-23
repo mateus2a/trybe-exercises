c61e1ea5e467e7f57b5574795fff3e50ff9be185
Mateus Alencar <mtauws@gmail.com>
2021-06-23 15:08:26 -0300

    feat: useEffect function

--- exercises/modulo_02/bloco_18/dia_03/hooks-fixation/src/App.js
+++ exercises/modulo_02/bloco_18/dia_03/hooks-fixation/src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-    </div>
+const App = () => {
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [numberIsMultiply, setNumberIsMultiply] = useState([]);
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    setInterval(() => {
+     const number = Math.floor(Math.random() * 100);
+     setRandomNumber(number);
+     
+      if (number % 5 === 0 || number % 3 === 0) {
+        setNumberIsMultiply(...numberIsMultiply, number);
+        setMessage('Acertou');
+        if (setMessage('Acertou')) {
+          setInterval(() => {
+            setMessage('');
+          }, 4000);
+        }
+      }
+    }, 10000);
+  }, []);
+
+  return  (
+    <>
+      <h1>{ randomNumber }</h1>
+      <p>{ message }</p>
+    </>
   );
 }
 
