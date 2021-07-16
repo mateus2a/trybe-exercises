32e834cc870697fbded1016e777eb2fa94db16a3
Mateus Alencar <mtauws@gmail.com>
2021-07-16 12:20:09 -0300

    chore: exercise three

---
+++ exercises/modulo_03/bloco_20/dia_02/exercise-three.js
@@ -0,0 +1,3 @@
+// SELECT CONCAT(title, ' ', release_year) AS 'Lançamento do Filme' FROM sakila.film;
+// SELECT CONCAT(title, ' ', rating) AS Classificação FROM sakila.film;
+// SELECT CONCAT(address, ', ', district) AS Endereço FROM sakila.address;
