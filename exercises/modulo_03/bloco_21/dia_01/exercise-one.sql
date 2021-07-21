de38795363c81d19b1d1b9416a4d6ce62ce8f35c
Mateus Alencar <mtauws@gmail.com>
2021-07-20 22:20:51 -0300

    chore: exercise 1

---
+++ exercises/modulo_03/bloco_21/dia_01/exercise-one.sql
@@ -0,0 +1,23 @@
+-- Exercise 1
+SELECT
+  UCASE('trybe');
+-- Exercise 2
+SELECT
+  REPLACE(
+    'Você já ouviu falar do DuckDuckGo?',
+    'DuckDuckGo',
+    'Google'
+  );
+-- Exercise 3
+SELECT
+  LENGTH('Uma frase qualquer');
+-- Exercise 4
+SELECT
+  SUBSTRING(
+    'A linguagem JavaScript está entre as mais usadas',
+    12,
+    11
+  );
+-- Exercise 5
+SELECT
+  LCASE('RUA NORTE 1500, SÃO PAULO, BRASIL');
