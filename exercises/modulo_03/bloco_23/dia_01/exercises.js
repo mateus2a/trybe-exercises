c073c507cacf8435ad6c69a9bdbab9a4d101908c
Mateus <mtauws@gmail.com>
2021-08-09 14:27:28 -0300

    chore: exercises 1-5

--- exercises/modulo_03/bloco_23/dia_01/exercises.js
+++ exercises/modulo_03/bloco_23/dia_01/exercises.js
@@ -1,9 +1,27 @@
 // Exercise 1
+/*
+  db.bios.find({ "_id": 8 }).pretty()
+*/
 // Exercise 2
+/*
+  db.bios.find({"_id": 8}, {"name": 1}).pretty()
+*/
 // Exercise 3
+/*
+  db.bios.find({"_id": 8}, {"name": 1, "birth": 1, "_id": 0}).pretty()
+*/
 // Exercise 4
+/*
+  db.bios.find({"name.first": "John"}).pretty()
+*/
 // Exercise 5
+/*
+  db.bios.find().limit(3).pretty()
+*/
 // Exercise 6
+/*
+  db.bios.find().limit(3).pretty()
+*/
 // Exercise 7
 // Exercise 8
 // Exercise 9
