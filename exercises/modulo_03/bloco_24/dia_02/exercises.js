1239ade0ec66bc41caa0d59750f3379ee57dfd7f
Mateus Alencar <mtauws@gmail.com>
2021-08-13 12:16:36 -0300

    chore: exercise 7-8

--- exercises/modulo_03/bloco_24/dia_02/exercises.js
+++ exercises/modulo_03/bloco_24/dia_02/exercises.js
@@ -28,3 +28,23 @@ db.movies.updateOne(
   { title: 'Batman' },
   { $push: { category: 'action' } }
 )
+// Exercise 7
+db.movies.updateMany(
+  { $or: [{title: 'Batman'}, {title: 'Home Alone'}]},
+  { $push: { category: "90's" } }
+)
+// Exercise 8
+db.movies.updateOne(
+  {title: 'Home Alone'},
+  {$push: { cast: { $each: [{
+    "actor": "Macaulay Culkin",
+    "character": "Kevin"
+  },
+  {
+    "actor": "Joe Pesci",
+    "character": "Harry"
+  },
+  {
+    "actor": "Daniel Stern"
+  }] } }}
+)
