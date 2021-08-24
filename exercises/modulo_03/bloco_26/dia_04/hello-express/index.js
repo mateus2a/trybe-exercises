3cc575fa2364cb7b4099e313c78199807989ce2c
Mateus Alencar <mtauws@gmail.com>
2021-08-24 15:33:24 -0300

    package: bodyParser

--- exercises/modulo_03/bloco_26/dia_04/hello-express/index.js
+++ exercises/modulo_03/bloco_26/dia_04/hello-express/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.json());
 
 app.get('/hello', (req, res) => {
   res.status(200).send(`Hello World`);
