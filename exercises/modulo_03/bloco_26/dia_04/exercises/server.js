8b3f45183d77fed738d3b9ea735bb19d321e0720
Mateus Alencar <mtauws@gmail.com>
2021-08-24 18:27:58 -0300

    chore: exercise 1

--- exercises/modulo_03/bloco_26/dia_04/exercises/server.js
+++ exercises/modulo_03/bloco_26/dia_04/exercises/server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// Exercise 1
+app.get('/ping', (req, res) => {
+  res.json( { message: 'pong' } );
+});
+
+app.listen(3333, (req, res) => {
+  console.log('Rodando na porta 3333');
+})
