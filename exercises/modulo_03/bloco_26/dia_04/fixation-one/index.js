430f4ac8bd425929b505a4bb297f34a60051e682
Mateus Alencar <mtauws@gmail.com>
2021-08-30 12:43:34 -0300

    chore: fixation 4

--- exercises/modulo_03/bloco_26/dia_04/fixation-one/index.js
+++ exercises/modulo_03/bloco_26/dia_04/fixation-one/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 
+const app = express();
+app.use(bodyParser.json());
 const recipes = [
   { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
   { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
@@ -26,6 +28,12 @@ app.get('/recipes', function (req, res) {
   res.json(recipes.sort(order));
 });
 
+app.post('/recipes', function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
 app.get('/drinks', function (req, res) {
   res.json(drinks.sort(order));
 });
@@ -37,6 +45,29 @@ app.get('/drinks/:id', function (req, res) {
   if(!recipe) res.status(404).json({ "error": "User not found" });
 
   res.status(200).json(recipe);
+});
+
+app.put('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipeIndex] = { ...recipes[recipeIndex], name, price };
+
+  res.status(204).end();
+});
+
+app.delete('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const recipeIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipeIndex === -1) return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes.splice(recipeIndex, 1);
+
+  res.status(204).end();
 });
 
 app.get('/recipes/search', function (req, res) {
