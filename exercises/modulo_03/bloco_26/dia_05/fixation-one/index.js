fd66ed6bfb6a0451aa9e5cad9923d2f1803632ee
Mateus Alencar <mtauws@gmail.com>
2021-08-30 14:23:02 -0300

    chore: create

--- exercises/modulo_03/bloco_26/dia_05/fixation-one/index.js
+++ exercises/modulo_03/bloco_26/dia_05/fixation-one/index.js
@@ -1,17 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const validatePrice = require('./middlewares/validationPrice');
 const recipes = require('./apis/recipes.json');
 
 const app = express();
 
 app.use(bodyParser.json());
 
+const validatePrice = (req, res, next) => {
+  const { price } = req.body;
+  if (price < 0) res.status(406).json({ "error": "Not Acceptable"});
+  if (isNaN(price)) res.status(400).json({ "error": "Bad Request"});
 
+  next();
+}
 
 app.post('/recipes', validatePrice, (req, res) => {
-
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price });
+  res.status(200).json(recipes);
 });
 
 app.put('/recipes/:id', validatePrice, (req, res) => {
