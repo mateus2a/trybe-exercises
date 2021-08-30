66e7fcab6fb22a133daee16e8fe64e7161fae2d0
Mateus Alencar <mtauws@gmail.com>
2021-08-30 12:24:30 -0300

    chore: fixation 1

--- exercises/modulo_03/bloco_26/dia_04/fixation-one/index.js
+++ exercises/modulo_03/bloco_26/dia_04/fixation-one/index.js
@@ -7,10 +7,29 @@ const recipes = [
   { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
 ];
 
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+const order = (a, b) => {
+  if (a.name < b.name) return -1;
+  if (a.name > b.name) return 1;
+  return 0;
+}
+
 app.get('/recipes', function (req, res) {
-  res.json(recipes);
+  res.json(recipes.sort(order));
 });
 
-app.listen(3001, () => {
+app.get('/drinks', function (req, res) {
+  res.json(drinks.sort(order));
+});
+
+app.listen(3333, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
