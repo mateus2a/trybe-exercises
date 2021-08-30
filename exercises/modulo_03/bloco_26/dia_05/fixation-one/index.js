ca09b810ab488f71303d81b22ea28f0ab612fb00
Mateus Alencar <mtauws@gmail.com>
2021-08-30 13:55:50 -0300

    chore: creation of fixation 1

---
+++ exercises/modulo_03/bloco_26/dia_05/fixation-one/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const recipes = require('./apis/recipes.json');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+const validatePrice = (req, res, next) => {
+  const { price } = req.body;
+  if (price < 0) res.status(406).json({ "error": "Not Acceptable"});
+  if (isNaN(price)) res.status(400).json({ "error": "Bad Request"});
+
+  next();
+}
+
+app.post('/recipes', validatePrice, (req, res) => {
+
+});
+
+app.put('/recipes/:id', validatePrice, (req, res) => {
+
+});
+
+app.listen(3333);
