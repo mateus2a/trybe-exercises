e02934845d6a3d0d779cd5fc43dbfb4878861a07
Mateus Alencar <mtauws@gmail.com>
2021-08-30 14:49:18 -0300

    chore: server.js

--- exercises/modulo_03/bloco_26/dia_05/fixation-one/index.js
+++ exercises/modulo_03/bloco_26/dia_05/fixation-one/index.js
@@ -1,36 +1,36 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+// const express = require('express');
+// const bodyParser = require('body-parser');
 
-const recipes = require('./apis/recipes.json');
+// const recipes = require('./apis/recipes.json');
 
-const app = express();
+// const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-const validatePrice = (req, res, next) => {
-  const { price } = req.body;
-  if (price < 0) res.status(406).json({ "error": "Not Acceptable"});
-  if (isNaN(price)) res.status(400).json({ "error": "Bad Request"});
+// const validatePrice = (req, res, next) => {
+//   const { price } = req.body;
+//   if (price < 0) res.status(406).json({ "error": "Not Acceptable"});
+//   if (isNaN(price)) res.status(400).json({ "error": "Bad Request"});
 
-  next();
-}
+//   next();
+// }
 
-app.get('/recipes', (req, res) => {
-  res.status(200).json(recipes);
-});
+// app.get('/recipes', (req, res) => {
+//   res.status(200).json(recipes);
+// });
 
-app.post('/recipes', validatePrice, (req, res) => {
-  const { id, name, price } = req.body;
-  recipes.push({ id, name, price });
-  res.status(200).json(recipes);
-});
+// app.post('/recipes', validatePrice, (req, res) => {
+//   const { id, name, price } = req.body;
+//   recipes.push({ id, name, price });
+//   res.status(200).json(recipes);
+// });
 
-app.put('/recipes/:id', validatePrice, (req, res) => {
-  const { id } = req.params;
-  const recipe = recipes.findIndex((r) => r.id = parseInt(id));
-  recipes.splice(recipe, 1);
+// app.put('/recipes/:id', validatePrice, (req, res) => {
+//   const { id } = req.params;
+//   const recipe = recipes.findIndex((r) => r.id = parseInt(id));
+//   recipes.splice(recipe, 1);
 
-  res.status(204).end();
-});
+//   res.status(204).end();
+// });
 
-app.listen(3333);
+// app.listen(3333);
