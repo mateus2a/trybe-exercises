a0098aea551a15fc197a7208136d31245f770b3d
Mateus Alencar <mtauws@gmail.com>
2021-09-02 14:38:09 -0300

    chore: method getAll in Book Model

--- exercises/modulo_03/bloco_27/dia_01/model-mysql/server.js
+++ exercises/modulo_03/bloco_27/dia_01/model-mysql/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const app = express();
 
 const Author = require('./src/models/Author');
+const Book = require('./src/models/Book');
 
 app.get('/authors', async (_req, res) => {
   const authors = await Author.getAll();
   return res.status(200).json(authors);
+});
+
+app.get('/books', async (_req, res) => {
+  const books = await Book.getAll();
+  return res.status(200).json(books);
 });
 
 app.listen(3000);
---
+++ exercises/modulo_03/bloco_27/dia_01/model-mysql/src/models/Book.js
@@ -0,0 +1,10 @@
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [books] = await connection.execute('SELECT id, title FROM books');
+  return books;
+};
+
+module.exports = {
+  getAll,
+};
