5d4d7bac433a3e529f49e5526ca2329e102a8f5e
Mateus Alencar <mtauws@gmail.com>
2021-09-03 15:48:21 -0300

    controllers and models

--- exercises/modulo_03/bloco_27/dia_02/cep-lookup/index.js
+++ exercises/modulo_03/bloco_27/dia_02/cep-lookup/index.js
@@ -1,10 +1,21 @@
+// index.js
+
+// Carregamos as variáveis de ambiente
+require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 
-const routes = require('./routes');
+// Criamos a aplicação do express
 const app = express();
-app.use(bodyParser.json());
 
-app.use(routes);
+// Registramos o endpoint `GET /ping`
+app.get('/ping', (req, res) => {
+  res.status(200).json({ message: 'pong!' });
+});
 
-app.listen(3000);
+// Lemos a porta da variável de ambiente, ou usamos 3000
+const PORT = process.env.PORT || 3000;
+
+// Iniciamos a aplicação ouvindo na porta informada na variável de ambiente.
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
--- exercises/modulo_03/bloco_27/dia_02/cep-lookup/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const router = express.Router();
-
-router.get('/ping', (_req, res) => {
-  res.status(200).json({ message: 'pong!' });
-});
-
-module.exports = router;
