66c33f11b5dff751fd513e9aef7a943d191ff48b
Mateus Alencar <mtauws@gmail.com>
2021-09-08 19:15:52 -0300

    chore: exercises

--- exercises/modulo_03/bloco_27/dia_03/refactoring/controllers/productController.js
+++ exercises/modulo_03/bloco_27/dia_03/refactoring/controllers/productController.js
@@ -3,38 +3,38 @@ const ProductModel = require('../models/productModel');
 
 const router = express.Router();
 
-router.get('/list-products', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   const products = await ProductModel.getAll();
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
-router.get('/get-by-id/:id', async (req, res, next) => {
+router.get('/products/:id', async (req, res, next) => {
   const product = await ProductModel.getById(req.params.id);
 
-  res.send(product);
+  res.status(200).json(product);
 });
 
-router.post('/add-user', async (req, res) => {
+router.post('/products', async (req, res) => {
   const { name, brand } = req.body;
 
   const newProduct = await ProductModel.add(name, brand);
 
-  res.send(newProduct);
+  res.status(201).json(newProduct);
 });
 
-router.post('/delete-user/:id', async (req, res) => {
+router.delete('/products/:id', async (req, res) => {
   const products = await ProductModel.exclude(req.params.id);
 
-  res.send(products);
+  res.status(204).json(products);
 });
 
-router.post('/update-user/:id', async (req, res) => {
+router.put('/products/:id', async (req, res) => {
   const { name, brand } = req.body;
 
   const products = await ProductModel.update(req.params.id, name, brand);
 
-  res.send(products);
+  res.status(200).json(products);
 });
 
 module.exports = router;
