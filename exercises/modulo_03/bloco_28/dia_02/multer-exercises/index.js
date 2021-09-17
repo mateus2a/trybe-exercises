27ad6043c458c0aa2315cfb81daa76cc04271e47
Mateus Alencar <mtauws@gmail.com>
2021-09-17 14:48:01 -0300

    chore: req 3

--- exercises/modulo_03/bloco_28/dia_02/multer-exercises/index.js
+++ exercises/modulo_03/bloco_28/dia_02/multer-exercises/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const fs = require('fs');
 
 const app = express();
 app.use(express.json());
+app.use(express.static(`${__dirname}/uploads`));
+const fileExists = (fileName) => {
+  //fs.readdirSync retorna uma lista com nome de todos os arquivos da pasta uploads.
+  const files = fs.readdirSync(`${__dirname}/uploads`);
+  //Aqui usamos a função some, que retorna `true` se algum dos items do array passar no teste, no nosso caso o `file.includes`.
+  return files.some(file => file === fileName);
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, callback) => {
@@ -18,6 +25,13 @@ const upload = multer({
   storage,
   fileFilter: (req, file, callback) => {
     if (file.mimetype == "image/png") {
+      if (fileExists(file.originalname)) {
+        //Colocar uma flag de erro na requisição
+        req.fileDuplicated = true;
+    
+        //Rejeitar o arquivo
+        return cb(null, false);
+      }
       callback(null, true);
     } else {
       callback(null, false);
@@ -27,6 +41,8 @@ const upload = multer({
  });
 
 app.post('/upload', upload.single('file'), (_req, res) => {
+  if (req.fileDuplicated)
+    return res.status(409).send({ error: { mesage: "File already exists" } })
   res.status(200).send('Ok');
 });
 
