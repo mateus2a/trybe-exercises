0273f7e1c7bc5dfa07111daa7ab14f1e50b36c88
Mateus Alencar <mtauws@gmail.com>
2021-09-17 12:53:24 -0300

    chore: axios - ping

--- exercises/modulo_03/bloco_28/dia_02/fixation-one/index.js
+++ exercises/modulo_03/bloco_28/dia_02/fixation-one/index.js
@@ -20,6 +20,10 @@ app.get('/', (req, res) => {
   res.send('Hello World')
 });
 
+app.get('/ping', (req, res) => {
+  res.send('Pong')
+});
+
 app.get('/envios', upload.single('file'), (req, res) => {
   res.send('Ok');
 });
