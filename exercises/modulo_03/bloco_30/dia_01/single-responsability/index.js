0c59796fbcbe803b90d6bf0164a05197d2249222
Mateus Alencar <mtauws@gmail.com>
2021-09-30 09:55:03 -0300

    UPDATE getLetterGrades

--- exercises/modulo_03/bloco_30/dia_01/single-responsability/index.js
+++ exercises/modulo_03/bloco_30/dia_01/single-responsability/index.js
@@ -1,18 +1,35 @@
-/* "Converter" */
-const percentageGradesIntoLetters = ({ name, disciplines }) => ({
+
+const GRADE_DICT = {
+  0.9: 'A',
+  0.8: 'B',
+  0.7: 'C',
+  0.6: 'D',
+  0.1: 'E',
+};
+
+const getGradeLetter = (gradeNumber) => {
+  let letterGrade = 'F';
+
+  for (let i = 0; i < gradeKeys.length; i += 1) {
+    if (gradeNumber >= gradeKeys[i]) {
+      letterGrade = GRADE_DICT[gradeKeys[i]];
+      break;
+    }
+  }
+
+  return letterGrade;
+};
+
+
+const getLetterGrades = ({ name, grade }) => ({
   name,
-  disciplines: disciplines.map(({ name, grade }) => {
-    let letterGrade;
+  grade,
+  letterGrade: getGradeLetter(grade)});
 
-    if (grade >= 0.9) letterGrade = 'A';
-    else if (grade >= 0.8) letterGrade = 'B';
-    else if (grade >= 0.7) letterGrade = 'C';
-    else if (grade >= 0.6) letterGrade = 'D';
-    else if (grade >= 0.1) letterGrade = 'E';
-    else letterGrade = 'F';
-
-    return { name, grade, letterGrade };
-  })});
+const percentageGradesIntoLetters = ({ name, disciplines, school }) => ({
+  name,
+  school,
+  disciplines: disciplines.map(getLetterGrades)});
 
 /* "Determinar" */
 const approvedStudents = ({ disciplines }) =>
@@ -45,4 +62,5 @@ module.exports = {
   approvedStudents,
   updateApprovalData,
   setApproved,
+  getLetterGrades
 };
