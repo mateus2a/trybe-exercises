fe22fdbf13a94418270e120ed7e4d63365cc3a15
Mateus Alencar <mtauws@gmail.com>
2021-10-02 20:01:44 -0300

    aaa

--- exercises/modulo_03/bloco_30/dia_03/1-1/index.js
+++ exercises/modulo_03/bloco_30/dia_03/1-1/index.js
@@ -1,6 +1,6 @@
 // index.js
 const express = require('express');
-const { Address, Employee } = require('./models');
+const { Address, Employee, Book, User } = require('./models');
 
 const app = express();
 
@@ -20,21 +20,34 @@ app.get('/employees', async (_req, res) => {
 app.get('/employees/:id', async (req, res) => {
   try {
     const { id } = req.params;
-    const employee = await Employee.findOne({
-      where: { id },
-      include: [
-        {
-          model: Address,
-          as: 'addresses',
-          attributes: { exclude: ['number'] },
-        },
-      ],
-    });
+    const employee = await Employee.findOne({ where: { id } });
 
     if (!employee)
       return res.status(404).json({ message: 'Funcionário não encontrado' });
 
+    if (req.query.includeAddresses === 'true') {
+      const addresses = await Address.findAll({ where: { employeeId: id } });
+      return res.status(200).json({ employee, addresses });
+    }
     return res.status(200).json(employee);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: 'Algo deu errado' });
+  }
+});
+
+app.get('/usersbooks/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findOne({
+      where: { userId: id },
+      include: [{ model: Book, as: 'books', through: { attributes: [] } }],
+    });
+
+    if (!user)
+      return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json(user);
   } catch (e) {
     console.log(e.message);
     res.status(500).json({ message: 'Algo deu errado' });
