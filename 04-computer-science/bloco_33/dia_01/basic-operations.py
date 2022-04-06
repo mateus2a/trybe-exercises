15a48a2732d67787569e136b388d433ec94217d0
Mateus <mtauws@gmail.com>
2022-04-06 15:04:32 -0300

    chore: exercises 1 - 4

---
+++ 04-computer-science/bloco_33/dia_01/basic-operations.py
@@ -0,0 +1,8 @@
+print(2 * 3)
+print(2 + 3)
+print(3 / 2)
+
+# raiz quadrada de 25. O operador `**` significa "elevado a"
+square_root = 25 ** (1/2)
+
+print(square_root + 1)  # saída: 6.0
---
+++ 04-computer-science/bloco_33/dia_01/fixation.py
@@ -0,0 +1,26 @@
+# Fixation 1
+a = 10
+b = 5
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a ** b)
+print(a % b)
+
+# Fixation 2
+hours = 6
+minutes = 6 * 60
+seconds = minutes * 60
+
+print(hours)
+print(minutes)
+print(seconds)
+
+# Fixation 4
+book = 24.20
+book_discount = (book * 0.4) - book
+ship_books = (0.75 * 59) + 3
+print(abs(book_discount * 60) + ship_books)
