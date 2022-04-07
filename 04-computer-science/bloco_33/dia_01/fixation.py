2354db701fcae24cdd95c897f146633089945cfd
Mateus <mtauws@gmail.com>
2022-04-07 13:01:52 -0300

    chore: dados embutidos

--- 04-computer-science/bloco_33/dia_01/fixation.py
+++ 04-computer-science/bloco_33/dia_01/fixation.py
@@ -24,3 +24,33 @@ book = 24.20
 book_discount = (book * 0.4) - book
 ship_books = (0.75 * 59) + 3
 print(abs(book_discount * 60) + ship_books)
+
+# Fixation 5
+trybe_course = ["Introdução", "Front-end", "Back-end"]
+trybe_course.append("Ciência da Computação")
+print(trybe_course)
+
+# Fixation 6
+trybe_course[0] = "Fundamentos"
+print(trybe_course)
+
+# Fixation 7
+var = set()
+var.add("Mateus")
+print(var)
+
+info = {
+    "personagem": "Margarida",
+    "origem": "Pato Donald",
+    "nota": "Namorada do personagem principal nos quadrinhos do Pato Donald",
+}
+
+# Fixation 9
+info["recorrente"] = "Sim"
+
+print(info)
+
+# Fixation 10
+del info["origem"]
+
+print(info)
